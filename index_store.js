bb3ad29bec124a7fb3de5eb31254e43403306629
itsmindz <66760987+itsmindz@users.noreply.github.com>
2020-08-03 19:37:27 +0700

    Add files via upload

---
+++ index_store.js
@@ -0,0 +1,32 @@
+import React ,{Component} from "react";
+
+export default class Store extends Component{
+    state={
+        showInfo:false
+    };
+    handleInfo=()=>{
+          this.setState({
+              showInfo:!this.state.showInfo
+          })
+    }
+    render(){
+      const {id,name,img,detail,address}=this.props.shop;
+      const {removeShop}=this.props;
+      return(
+            <div className="shop">
+                <div className="img-container">
+                  <img src={img}/>
+                  <span className="close-btn" onClick={()=>removeShop(id)}>
+                      <i className="fas fa-window-close"></i>
+                  </span>
+                </div>
+                <div className="shop-info">
+                    <h4>{name}</h4>
+                    <h5>{detail}</h5>
+                    {this.state.showInfo && <p>{address}</p>}
+                </div>
+            </div>
+      );
+    }
+
+}
